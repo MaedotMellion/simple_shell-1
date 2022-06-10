f05ab9986cf26ecd925fd912fd195e8e1693a53b
Iwamgad <dagmawiharry2@gmail.com>
2022-06-10 00:16:28 -0700

    add trial directory

---
+++ trial/prompt.c
@@ -0,0 +1,11 @@
+#include "shell.h"
+
+void print_prompt1(void)
+{
+    _printf("$ ");
+}
+
+void print_prompt2(void)
+{
+    _printf("> ");
+}
---
+++ trial/shell.h
@@ -0,0 +1,19 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+void print_prompt1(void);
+void print_prompt2(void);
+char *read_cmd(void);
+int str_cmp (char *s1, char *s2);
+int _putchar(char c);
+void _printf (char *str);
+unsigned int _strlen(char *str);
+void _strcpy (char *str_dest, char *str_src);
+
+#endif /* SHELL_H */
